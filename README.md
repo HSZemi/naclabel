4598d8bf53b85d34e43d872aaee61470f4929a06
HSZemi <HSZemi@users.noreply.github.com>
2019-12-18 17:38:16 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # naclabel
 Name label generator for NAC3 public viewing and Meet &amp; Greet
+
+
+Make sure the `data` directory is writable for your webserver process.
---
+++ api.php
@@ -0,0 +1,44 @@
+<?php
+header('Content-type: application/json');
+if (isset($_GET['save'])
+    && isset($_POST['username'])
+    && isset($_POST['usernameSize'])
+    && isset($_POST['languages'])
+    && isset($_POST['tagline'])) {
+    $number = rand(10000, 99999);
+    $filepath = 'data/' . $number . '.json';
+    $success = false;
+    for ($i = 0; $i = 10; $i++) {
+        if (!file_exists($filepath)) {
+            $success = true;
+            break;
+        } else {
+            $number = rand(10000, 99999);
+            $filepath = 'data/' . $number . '.json';
+        }
+    }
+    if (!$success) {
+        echo '{"result":"error","message":"could not generate number :("}';
+        die();
+    }
+    file_put_contents($filepath, json_encode(array(
+        'username' => $_POST['username'],
+        'usernameSize' => $_POST['usernameSize'],
+        'languages' => $_POST['languages'],
+        'tagline' => $_POST['tagline']
+    )));
+    echo '{"result":"ok","message":"Your number: ' . $number . '"}';
+} elseif (isset($_GET['load'])) {
+    $number = intval($_GET['load']);
+    $filepath = 'data/' . $number . '.json';
+    if (file_exists($filepath)) {
+        $content = file_get_contents($filepath);
+        echo $content;
+    } else {
+        echo '{"result":"error","message":"No data for number ' . $number . '"}';
+    }
+} else {
+    echo '{"result":"error","message":"Invalid operation"}';
+}
+
+?>
